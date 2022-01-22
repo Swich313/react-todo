9b281d0fd637c7cdd539ebec191f689e2c270ad3
Andrey <andruha313@gmail.com>
2022-01-22 18:37:35 +0200

    todo done with toggle error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from "react";
+import TodoList from "./components/TodoList"
+import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [todos, setTodos] = useState([])
+ const todoNameRef = useRef()
+
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if (storedTodos) setTodos(storedTodos)
+}, []);
+
+useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+}, [todos]);
+
+ function toggleTodo(id) {
+  const newTodos = [...todos]
+  const todo = newTodos.find(item => {
+    if (item.id === id){
+      item.complete = !item.complete
+      setTodos(newTodos)
+    }
+  })
+ }
+
+ function handleAddTodo (e) {
+   const name = todoNameRef.current.value
+   if (name === '') {return} 
+    console.log(name)
+    setTodos(prevTodo => {
+      return [...prevTodo, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+ }
+
+function handleDeleteTodos () {
+  const newTodos = todos.filter(item => !item.complete)
+  setTodos(newTodos)
+}
+
+
+return (
+    <>
+    <TodoList todoList={todos} toggleTodo={toggleTodo}/>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add todo</button>
+    <button onClick={handleDeleteTodos}>Clear completed todos</button>
+    <div>{todos.filter(item => !item.complete).length} left to do</div>
+    </>
+    
+  )
 }
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList({todoList, toggleTodo}) {
+    return (
+       todoList.map(i =>{
+        return <Todo key={i.id} todo={i} toggleTodo={toggleTodo}/>
+       })
+       
+    )
+}
